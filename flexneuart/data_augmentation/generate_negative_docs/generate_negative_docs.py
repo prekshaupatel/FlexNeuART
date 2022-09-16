99593b90010b653bac722b1c15a9709c388f237d
riddhinn99 <riddhinisar11@gmail.com>
2022-09-15 22:20:12 -0400

    add code for 3 output files

--- flexneuart/data_augmentation/generate_negative_docs/generate_negative_docs.py
+++ flexneuart/data_augmentation/generate_negative_docs/generate_negative_docs.py
@@ -1,15 +1,18 @@
-from flexneuart.retrieval.cand_provider import *
 import random
 import argparse
 import os
 import json
+import csv
+from flexneuart.retrieval.cand_provider import *
 from flexneuart import configure_classpath
 from flexneuart.retrieval import create_featextr_resource_manager
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, required=True)
-    parser.add_argument('--output', type=str, required=True)
+    parser.add_argument('--output_data', type=str, required=True)
+    parser.add_argument('--output_train_pairs', type=str, required=True)
+    parser.add_argument('--output_qrels', type=str, required=True)
     # parser.add_argument('--output_ids', type=str, required=True)
     # parser.add_argument('--corpus', type=str, required=True)
     parser.add_argument('--index', type=str, default='msmarco-passage')
@@ -31,9 +34,12 @@ if __name__ == '__main__':
     cand_prov = create_cand_provider(resource_manager, PROVIDER_TYPE_LUCENE, args.index)
 
 
-    with open(args.input) as new_queries_file, open(args.output, 'w') as new_triples_file, open(args.output_ids, 'w') as fout_ids:
+    with open(args.input) as new_queries_file, open(args.output_data, 'w') as output_data, open(args.output_train_pairs, 'w') as output_train_pairs, open(args.output_qrels, 'w') as output_qrels:
         json_array = json.load(new_queries_file)
-        
+        tsv_writer = csv.writer(output_data, delimiter='\t')
+        tsv_writer_pairs = csv.writer(output_train_pairs, delimiter='\t')
+        tsv_writer_qrels = csv.writer(output_qrels, delimiter='\t')
+
         for query_line in json_array:
             new_query = query_line['question']
             positive_doc_id = query_line['doc_id']
@@ -46,8 +52,32 @@ if __name__ == '__main__':
             negative_doc = query_response[0][random_number]
             negative_doc_id = negative_doc.doc_id
 
-            # TSV -> query/doc, id, string of query/doc
-            new_triples_file.write(f'{new_query}\t{positive_doc_id}\t{negative_doc_id}\n')
+            # TODO: get it from code
+            negative_doc_text = "get_negative_doc_text"
 
+            # set query id to a random large number
+            query_id = random.randint(670000, 6700000)
+            
+            query_row = ['query']
+            query_row.append(query_id)
+            query_row.append(new_query)
+            tsv_writer.writerow(query_row)
+
+            doc_row_neg = ['doc']
+            doc_row_neg.append(negative_doc_id)
+            doc_row_neg.append(negative_doc_text)
+            tsv_writer.writerow(doc_row_neg)
+
+            doc_row_pos = ['doc']
+            doc_row_pos.append(positive_doc_id)
+            doc_row_pos.append(positive_doc_text)
+            tsv_writer.writerow(doc_row_pos)
+
+            tsv_writer_pairs.writerow([query_id, positive_doc_id])
+            tsv_writer_pairs.writerow([query_id, negative_doc_id])
+
+            tsv_writer_qrels.writerow([query_id, 0, positive_doc_id, 1])
+            tsv_writer_qrels.writerow([query_id, 0, negative_doc_id, -1])
+            
         
         
