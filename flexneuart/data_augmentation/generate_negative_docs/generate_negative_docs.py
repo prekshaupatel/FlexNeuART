416437634e9e288ef5bfa0d68d5c9fac15e05d37
Ubuntu <ubuntu@ip-172-31-80-234.ec2.internal>
2022-09-20 04:16:32 +0000

    Fix negative doc generation

--- flexneuart/data_augmentation/generate_negative_docs/generate_negative_docs.py
+++ flexneuart/data_augmentation/generate_negative_docs/generate_negative_docs.py
@@ -3,9 +3,18 @@ import argparse
 import os
 import json
 import csv
-from flexneuart.retrieval.cand_provider import *
+import pdb
+
 from flexneuart import configure_classpath
 from flexneuart.retrieval import create_featextr_resource_manager
+from flexneuart.retrieval.fwd_index import get_forward_index
+
+COLLECTION = os.getenv('COLLECTION')
+COLLECTION_ROOT = os.environ.get('COLLECTION_ROOT')
+
+configure_classpath()
+
+from flexneuart.retrieval.cand_provider import *
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -15,49 +24,53 @@ if __name__ == '__main__':
     parser.add_argument('--output_qrels', type=str, required=True)
     # parser.add_argument('--output_ids', type=str, required=True)
     # parser.add_argument('--corpus', type=str, required=True)
-    parser.add_argument('--index', type=str, default='msmarco-passage')
+    parser.add_argument('--index', type=str, default='')
     # parser.add_argument('--max_hits', type=int, default=1000)
     # parser.add_argument('--n_samples', type=int, default=100)
+    parser.add_argument('--max_docs_retrieved', type=int, default=100)
+    parser.add_argument('--new_query_id', type=int, default=670000)
     args = parser.parse_args()
 
-    # Get environment variables
-    COLLECTION = os.getenv('COLLECTION')
-    COLLECTION_ROOT = os.environ.get('COLLECTION_ROOT')
-
-    # add Java JAR to the class path --> for setting java classpath
-    configure_classpath()
-
     # create a resource manager
-    resource_manager=create_featextr_resource_manager(resource_root_dir=f'{COLLECTION_ROOT}/{COLLECTION}/') # optional params -> fwd_index_dir='forward_index', model1_root_dir=f'derived_data/giza', embed_root_dir=f'derived_data/embeddings')
+    resource_manager=create_featextr_resource_manager(resource_root_dir=f'{COLLECTION_ROOT}/{COLLECTION}/', fwd_index_dir='forward_index', model1_root_dir=f'derived_data/giza', embed_root_dir=f'derived_data/embeddings')
                                                   
     # create a candidate provider/generator
     cand_prov = create_cand_provider(resource_manager, PROVIDER_TYPE_LUCENE, args.index)
-
+    generated_query_id = args.new_query_id
 
     with open(args.input) as new_queries_file, open(args.output_data, 'w') as output_data, open(args.output_train_pairs, 'w') as output_train_pairs, open(args.output_qrels, 'w') as output_qrels:
-        json_array = json.load(new_queries_file)
+        #json_array = json.load(new_queries_file
         tsv_writer = csv.writer(output_data, delimiter='\t')
         tsv_writer_pairs = csv.writer(output_train_pairs, delimiter='\t')
         tsv_writer_qrels = csv.writer(output_qrels, delimiter='\t')
 
-        for query_line in json_array:
+        for line in new_queries_file:
+            query_line = json.loads(line)
             new_query = query_line['question']
             positive_doc_id = query_line['doc_id']
             positive_doc_text = query_line['doc_text']
-            log_probs = query_line['log_probs']
+            #log_probs = query_line['log_probs']
 
-            query_response = run_text_query(cand_prov, 20, new_query) # response = (1329, [CandidateEntry(doc_id='639661', score=18.328275680541992),...])
+            query_response = run_text_query(cand_prov, args.max_docs_retrieved, new_query) # response = (1329, [CandidateEntry(doc_id='639661', score=18.328275680541992),...])
             no_of_responses =  query_response[0]
-            random_number = random.randint(0, no_of_responses)
-            negative_doc = query_response[0][random_number]
+            if no_of_responses == 0:
+                continue
+            #pdb.set_trace()
+            print("No of responses: "+str(no_of_responses))
+            random_number = random.randint(0, min(no_of_responses, args.max_docs_retrieved))
+            print("Random number: "+ str(random_number))
+            negative_doc = query_response[1][random_number]
             negative_doc_id = negative_doc.doc_id
 
             # TODO: get negative_doc_text from code using doc_id
-            fwd_index = resource_manager.getFwdIndex('text')
-            negative_doc_text = fwd_index.getDocEntryTextRaw(negative_doc_id)
+            #fwd_index = resource_manager.getFwdIndex('text_raw')
+            raw_index = get_forward_index(resource_manager, 'text_raw')
+            negative_doc_text = raw_index.get_doc_text_raw(negative_doc_id)
+            #negative_doc_text = fwd_index.getDocEntryTextRaw(negative_doc_id)
 
             # set query id to a random large number
-            query_id = random.randint(670000, 6700000)
+            generated_query_id = generated_query_id + 1
+            query_id = random.randint(generated_query_id, generated_query_id)
             
             query_row = ['query']
             query_row.append(query_id)
