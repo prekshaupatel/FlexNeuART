70546dc26db956fa11341de9557b79e50ccd720b
Vivek Sourabh <viveksourabh2808@gmail.com>
2022-10-01 20:04:28 -0400

    Updated file structure for integration

--- flexneuart/data_augmentation/generate_negative_docs/generate_negative_docs.py
+++ flexneuart/data_augmentation/generate_negative_docs/generate_negative_docs.py
@@ -4,101 +4,96 @@ import os
 import json
 import csv
 import sys
+import time
 
 from flexneuart import configure_classpath
 from flexneuart.retrieval import create_featextr_resource_manager
 from flexneuart.retrieval.fwd_index import get_forward_index
 
-COLLECTION = os.getenv('COLLECTION')
-COLLECTION_ROOT = os.environ.get('COLLECTION_ROOT')
-
-if COLLECTION=='':
-    print("Please export COLLECTION. Example -> export COLLECTION=msmarco_pass")
-    sys.exit(1)
-
-if COLLECTION_ROOT=='':
-    print("Please export COLLECTION_ROOT. Example -> export COLLECTION_ROOT=/home/ubuntu/efs/capstone/data")
-    sys.exit(1)
-
-
 configure_classpath()
-
 from flexneuart.retrieval.cand_provider import *
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=str, required=True)
-    parser.add_argument('--output_data', type=str, required=True)
-    parser.add_argument('--output_train_pairs', type=str, required=True)
-    parser.add_argument('--output_qrels', type=str, required=True)
-    # parser.add_argument('--output_ids', type=str, required=True)
-    # parser.add_argument('--corpus', type=str, required=True)
-    parser.add_argument('--index', type=str, default='')
-    # parser.add_argument('--max_hits', type=int, default=1000)
-    # parser.add_argument('--n_samples', type=int, default=100)
-    parser.add_argument('--max_docs_retrieved', type=int, default=100)
-    parser.add_argument('--new_query_id', type=int, default=670000)
-    args = parser.parse_args()
+class BM25Retriever():
+    def __init__(self, args):
+        self.args = args
+        
+        collection, collection_root = self.__sanity_check()
 
-    # create a resource manager
-    resource_manager=create_featextr_resource_manager(resource_root_dir=f'{COLLECTION_ROOT}/{COLLECTION}/', fwd_index_dir='forward_index', model1_root_dir=f'derived_data/giza', embed_root_dir=f'derived_data/embeddings')
-                                                  
-    # create a candidate provider/generator
-    cand_prov = create_cand_provider(resource_manager, PROVIDER_TYPE_LUCENE, args.index)
-    generated_query_id = args.new_query_id
+        self.resource_manager = create_featextr_resource_manager(
+            resource_root_dir=f'{collection_root}/{collection}/',
+            fwd_index_dir='forward_index', model1_root_dir=f'derived_data/giza',
+            embed_root_dir=f'derived_data/embeddings')
 
-    query_rows = []
-    doc_row_negs = []
+        self.cand_prov = create_cand_provider(self.resource_manager, PROVIDER_TYPE_LUCENE, args.index)
+        self.counter = 0
+        self.timestamp = time.time()
+
+    def __call__(self, query):
+
+        query_response = run_text_query(self.cand_prov, self.args.max_docs_retrieved, query)
+        no_of_responses =  query_response[0]
+
+        if no_of_responses == 0:
+            return None, None
+
+        random_number = random.randint(0, min(no_of_responses, self.args.max_docs_retrieved))
+
+        negative_doc = query_response[1][random_number]
+        negative_doc_id = negative_doc.doc_id
+
+        raw_index = get_forward_index(self.resource_manager, 'text_raw')
+        negative_doc_text = raw_index.get_doc_text_raw(negative_doc_id)
+
+        query_id = self.__generate_unique_qid()
+
+        return query_id, negative_doc_id, negative_doc_text
+
+
+    def __generate_unique_qid(self):
+        qid = str(self.timestamp)  + '_' + str(self.counter)
+        self.counter += 1
+        return qid 
+
+    def __sanity_check(self, collection, collection_root):
+
+        collection = os.getenv("COLLECTION")
+        collection_root = os.getenv("COLLECTION_ROOT")
+
+        if collection=='' or collection==None:
+            print("Please export COLLECTION. Example -> export COLLECTION=msmarco_pass")
+            sys.exit(1)     
+        
+        if collection_root=='' or collection_root==None:
+            print("Please export COLLECTION_ROOT. Example -> export COLLECTION_ROOT=/home/ubuntu/efs/capstone/data")
+            sys.exit(1)
+        
+        return collection, collection_root
+
+
+def generate_negative(args):
     
+    new_queries_file = open(args.aug_query)
+    output_data = open(args.neg_doc, 'w')
+    output_qrels = open(args.neg_doc_qrels, 'w')
 
-    with open(args.input) as new_queries_file, open(args.output_data, 'w') as output_data, open(args.output_train_pairs, 'w') as output_train_pairs, open(args.output_qrels, 'w') as output_qrels:
-        #json_array = json.load(new_queries_file
-        tsv_writer = csv.writer(output_data, delimiter='\t')
-        tsv_writer_pairs = csv.writer(output_train_pairs, delimiter='\t')
-        tsv_writer_qrels = csv.writer(output_qrels, delimiter='\t')
+    tsv_writer = csv.writer(output_data, delimiter='\t')
+    tsv_writer_qrels = csv.writer(output_qrels, delimiter='\t')
 
-        for line in new_queries_file:
-            query_line = json.loads(line)
-            new_query = query_line['question']
-            positive_doc_id = query_line['doc_id']
-            positive_doc_text = query_line['doc_text']
-            #log_probs = query_line['log_probs']
+    bm25_retriever = BM25Retriever(args)
 
-            query_response = run_text_query(cand_prov, args.max_docs_retrieved, new_query) # response = (1329, [CandidateEntry(doc_id='639661', score=18.328275680541992),...])
-            no_of_responses =  query_response[0]
-            if no_of_responses == 0:
-                continue
-            #pdb.set_trace()
-            print("No of responses: "+str(no_of_responses))
-            random_number = random.randint(0, min(no_of_responses, args.max_docs_retrieved))
-            print("Random number: "+ str(random_number))
-            negative_doc = query_response[1][random_number]
-            negative_doc_id = negative_doc.doc_id
+    for line in new_queries_file:
+        query = json.loads(line)['question']
 
-            # TODO: get negative_doc_text from code using doc_id
-            #fwd_index = resource_manager.getFwdIndex('text_raw')
-            raw_index = get_forward_index(resource_manager, 'text_raw')
-            negative_doc_text = raw_index.get_doc_text_raw(negative_doc_id)
-            #negative_doc_text = fwd_index.getDocEntryTextRaw(negative_doc_id)
-
-            # set query id to a random large number
-            generated_query_id = generated_query_id + 1
-            query_id = random.randint(generated_query_id, generated_query_id)
-            
-            query_row = ['query', query_id, new_query]
-            tsv_writer.writerow(query_row)
-
-            doc_row_neg = ['doc', negative_doc_id, negative_doc_text]
-            tsv_writer.writerow(doc_row_neg)
-
-            doc_row_pos = ['doc', positive_doc_id, positive_doc_text]
-            tsv_writer.writerow(doc_row_pos)
-
-            tsv_writer_pairs.writerow([query_id, positive_doc_id])
-            tsv_writer_pairs.writerow([query_id, negative_doc_id])
-
-            tsv_writer_qrels.writerow([query_id, 0, positive_doc_id, 1])
-            tsv_writer_qrels.writerow([query_id, 0, negative_doc_id, -1])
-            
+        query_id, negative_doc_id, negative_doc_text = bm25_retriever(query)
         
-        
+        query_row = ['query', query_id, query]
+        tsv_writer.writerow(query_row)
+
+        doc_row_neg = ['doc', negative_doc_id, negative_doc_text]
+        tsv_writer.writerow(doc_row_neg)
+
+        tsv_writer_qrels.writerow([query_id, 0, negative_doc_id, -1])
+    
+    new_queries_file.close()
+    output_data.close()
+    output_qrels.close()
